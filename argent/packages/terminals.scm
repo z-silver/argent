2ce92996a49800938d2689c9ed7ace64ca64ea7a
Marinho <marinho.pinotti@gmail.com>
2024-07-20 23:43:14 -0300

    Add custom sakura

---
+++ argent/packages/terminals.scm
@@ -0,0 +1,15 @@
+(define-module (argent packages terminals)
+  #:use-module (gnu packages terminals)
+  #:use-module (guix gexp)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public custom-sakura
+  (package
+    (inherit sakura)
+    (name "sakura-argent")
+    (source
+      (origin
+        (inherit (package-source sakura))
+        (patches (list (local-file "../patches/sakura-argent.patch")))))))
