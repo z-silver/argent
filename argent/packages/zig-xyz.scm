14abd28730aefa8acd547e4d703da7777c629326
Marinho <marinho.pinotti@gmail.com>
2026-01-17 00:20:48 -0300

    Add xit

--- argent/packages/zig-xyz.scm
+++ argent/packages/zig-xyz.scm
@@ -91,3 +91,93 @@ to compare the performance of multiple commands with a colorful terminal user
 interface.")
       (home-page "https://github.com/andrewrk/poop")
       (license license:expat))))
+
+(define-public zig-xitdb
+  (let ((commit "43d88506d847237a6c9c5dc4466971f23cfefe03")
+        (revision "0"))
+    (package
+      (name "zig-xitdb")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/radarroark/xitdb")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0zz64pz260f50fwgmzvgx8d828dpgl92v20y8pb5ij4p7n9czzz8"))))
+      (build-system zig-build-system)
+      (arguments
+       (list
+        #:zig zig-0.15
+        #:skip-build? #t))
+      (synopsis "Immutable database for Zig")
+      (description
+       "xitdb is an immutable database written in Zig.
+
+Each transaction efficiently creates a new \"copy\" of the database, and past
+copies can still be read from.  It supports writing to a file as well as purely
+in-memory use.  No query engine of any kind.  You just write data structures
+(primarily an ArrayList and HashMap) that can be nested arbitrarily.  No
+dependencies besides the Zig standard library (requires version 0.15.1).")
+      (home-page "https://github.com/radarroark/xitdb")
+      (license license:expat))))
+
+(define-public zig-xitui
+  (let ((commit "3fb562d72a766eb1c34fb47f8279de663b86c184")
+        (revision "0"))
+    (package
+      (name "zig-xitui")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/radarroark/xitui")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "10z3hgcrkvdw6ssraldc0l0fnrcybvr35zmdqlfbifqk1swl2j0d"))))
+      (build-system zig-build-system)
+      (arguments
+       (list
+        #:zig zig-0.15
+        #:skip-build? #t))
+      (synopsis "TUI library for Zig")
+      (description
+       "xitui is a library for making TUIs in Zig (requires version 0.15.1).
+
+Includes a widget and focus system.  Widgets are put in a union type defined
+by the user, rather than using dynamic dispatch.  Supports Windows, MacOS and
+Linux.")
+      (home-page "https://github.com/radarroark/xitui")
+      (license license:expat))))
+
+(define-public xit
+  (let ((commit "1125172dae0340a91e6ee7f0f83f0ce29679c702")
+        (revision "0"))
+    (package
+      (name "xit")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/radarroark/xit")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "14gb2i8g5dxw16433vhpmfyrvzmw17a8jpn000dp3lh5yms2kjxi"))))
+      (build-system zig-build-system)
+      (arguments
+       (list
+        #:zig zig-0.15
+        #:zig-release-type "safe"
+        #:install-source? #f))
+      (inputs (list zig-xitdb zig-xitui))
+      (synopsis "Git alternative written in Zig")
+      (description
+       "Xit is a new version control system.  It's pronounced like \"zit\".")
+      (home-page "https://github.com/radarroark/xitui")
+      (license license:expat))))
